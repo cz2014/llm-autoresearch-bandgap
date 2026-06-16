7b5b116214ebee996e88fe7dd64c26a483cfc9d1
cz <cz@cz-3950x.taile7aa6a.ts.net>
2026-06-16 13:28:34 -0500

    A4: dim 96 -> 112 with element-pair edge

--- train.py
+++ train.py
@@ -270,7 +270,7 @@ class CrystalGNN(nn.Module):
         return self.readout(hg).squeeze(-1)
 
 
-model = CrystalGNN(num_elem=len(ELEMENTS), dim=96, n_layers=4, n_rbf=32, cutoff=CUTOFF).to(DEVICE)
+model = CrystalGNN(num_elem=len(ELEMENTS), dim=112, n_layers=4, n_rbf=32, cutoff=CUTOFF).to(DEVICE)
 ema_model = copy.deepcopy(model).to(DEVICE)
 for p in ema_model.parameters():
     p.requires_grad_(False)
